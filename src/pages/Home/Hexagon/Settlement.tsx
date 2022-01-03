9759045fb690c36c9d42fb9551c520ec6a973311
Mike Riddelsdell <mikerid94@gmail.com>
2022-01-03 18:32:23 +0000

    allow users to add settlements to the board

--- src/pages/Home/Hexagon/Settlement.tsx
+++ src/pages/Home/Hexagon/Settlement.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled, { css, FlattenSimpleInterpolation } from "styled-components";
+import { toggleSettlement } from "../../../redux/settlements/settlements.slice";
 import { State } from "../../../redux/store";
 
 const settlementIndexMap: { [index: number]: FlattenSimpleInterpolation } = {
@@ -31,26 +32,42 @@ const settlementIndexMap: { [index: number]: FlattenSimpleInterpolation } = {
 };
 
 const playerMap: { [index: number]: string } = {
+  0: "none",
   1: "white",
   2: "red",
   3: "blue",
 };
 
-const House = styled.div<{ settlementIndex: number; player: number }>`
+const House = styled.div<{
+  settlementIndex: number;
+  player: number;
+  type: "settlement" | "city" | undefined;
+}>`
   height: 20px;
   width: 20px;
   background-color: ${(p) => playerMap[p.player]};
   position: absolute;
   ${(p) => settlementIndexMap[p.settlementIndex]}
-  border: thin solid black;
+  ${(p) => p.player !== 0 && `border: thin solid black;`}
+  cursor: pointer;
+  ${(p) => p.type === "settlement" && `border-radius: 100%`}
 `;
 
 export const Settlement: FunctionComponent<{
   settlementIndex: number;
   node: `${number}-${number}-${number}`;
 }> = ({ settlementIndex, node }) => {
+  const dispatch = useDispatch();
   const nodes = useSelector((state: State) => state.settlements);
   const settlement = nodes.find((x) => x.position === node);
-  if (settlement === undefined) return null;
-  return <House settlementIndex={settlementIndex} player={settlement.player} />;
+  return (
+    <House
+      onClick={() => {
+        dispatch(toggleSettlement(node));
+      }}
+      settlementIndex={settlementIndex}
+      player={settlement?.player ?? 0}
+      type={settlement?.type}
+    />
+  );
 };
