8f83a742bb4bb4c7013f73142adef9ac26caff8d
Mike Riddelsdell <mikerid94@gmail.com>
2022-01-03 16:36:38 +0000

    add default settlements

--- src/redux/settlements/settlements.slice.ts
+++ src/redux/settlements/settlements.slice.ts
@@ -6,7 +6,12 @@ interface Settlement {
   type: "settlement" | "city";
 }
 const initialState: Settlement[] = [
-  { position: "8-13-14", player: 1, type: "settlement" },
+  { position: "11-12-18", player: 1, type: "settlement" },
+  { position: "20-21-27", player: 1, type: "settlement" },
+  { position: "8-13-14", player: 2, type: "settlement" },
+  { position: "25-26-31", player: 2, type: "settlement" },
+  { position: "24-25-30", player: 3, type: "settlement" },
+  { position: "26-27-32", player: 3, type: "settlement" },
 ];
 
 export const SettlementsSlice = createSlice({
