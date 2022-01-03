1e16108cd8e5596d7b619cbe2e29327f77b2c6b3
Mike Riddelsdell <mikerid94@gmail.com>
2022-01-03 16:34:09 +0000

    add dummy settlement

--- src/redux/settlements/settlements.slice.ts
+++ src/redux/settlements/settlements.slice.ts
@@ -3,9 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 interface Settlement {
   position: string;
   player: number;
-  type: "settlment" | "city";
+  type: "settlement" | "city";
 }
-const initialState: Settlement[] = [];
+const initialState: Settlement[] = [
+  { position: "8-13-14", player: 1, type: "settlement" },
+];
 
 export const SettlementsSlice = createSlice({
   name: "Hexagons",
